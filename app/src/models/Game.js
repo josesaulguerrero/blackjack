ab613321a4d9321345b2dc0c1f8368ff95be7b03
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-11 16:11:45 -0500

    designed the Game class

--- app/src/models/Game.js
+++ app/src/models/Game.js
@@ -1,5 +1,22 @@
 export class Game {
+	#round;
+	#isInitialized = false;
+
+	constructor() {
+		if (this.#isInitialized) {
+			throw new Error(
+				"This is a singleton method, use #getInstance instead."
+			);
+		}
+	}
+
 	static getInstance() {
-		return new Game();
+		const instance = new Game();
+		this.#isInitialized = true;
+		return instance;
+	}
+
+	get round() {
+		return this.round;
 	}
 }
