a66087c72a3b8320c0bdb9ce8159032a30de73cd
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-11 16:12:01 -0500

    designed the Card class

---
+++ app/src/models/Card.js
@@ -0,0 +1,23 @@
+export class Card {
+	#suit;
+	#name;
+	#value;
+
+	constructor(suit, name, value) {
+		this.#suit = suit;
+		this.#name = name;
+		this.#value = value;
+	}
+
+	get suit() {
+		return this.#suit;
+	}
+
+	get name() {
+		return this.#name;
+	}
+
+	get value() {
+		return this.#value;
+	}
+}
