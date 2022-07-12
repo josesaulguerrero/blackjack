5f347e488933f36681c067e82862d7860c5bd4ee
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-12 08:46:23 -0500

    created a NumberUtils class to abstract numerical validations

---
+++ app/src/utils/NumberUtils.js
@@ -0,0 +1,13 @@
+export class NumberUtils {
+	static isBetweenRange(value, lowerLimit, upperLimit) {
+		if (upperLimit < lowerLimit)
+			throw new Error(
+				"The upper limit must be greater than the lower limit"
+			);
+		return value >= lowerLimit && value <= upperLimit;
+	}
+
+	static isGreaterThanOrEqual(value, base) {
+		return value >= base;
+	}
+}
