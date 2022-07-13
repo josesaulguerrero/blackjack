4c4a0048f1dfddf27bda208f09e5f1f3fab94b28
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-12 19:04:42 -0500

    replaced readline by inquirer and implemented the #stand and #hit methods on Game

--- app/index.js
+++ app/index.js
@@ -1,12 +1,30 @@
+import inquirer from "inquirer";
 import { instance as game } from "./src/models/Game.js";
 import { ConsoleUtils } from "./src/utils/ConsoleUtils.js";
 
+// const handleInitialInput = (option) => {
+// 	/y(es)?/i.test(option.trim()) ? game.start() : game.exit();
+// };
+
+// ConsoleUtils.readAndVerify(
+// 	"Are you ready to play 🃏Blackjack🃏?! (Y/n)",
+// 	handleInitialInput,
+// 	(value) => /^(y(es)?|n(o)?)$/i.test(value.trim())
+// );
 const handleInitialInput = (option) => {
-	/y(es)?/i.test(option.trim()) ? game.start() : game.exit();
+	option ? game.start() : game.exit();
 };
 
-ConsoleUtils.readAndVerify(
-	"Are you ready to play 🃏Blackjack🃏?! (Y/n)",
-	handleInitialInput,
-	(value) => /^(y(es)?|n(o)?)$/i.test(value.trim())
-);
+(async () => {
+	const option = await inquirer
+		.prompt([
+			{
+				message: "Are you ready to play 🃏Blackjack🃏?!",
+				name: "startGame",
+				type: "confirm",
+				validate: (value) => /^(y(es)?|n(o)?)$/i.test(value.trim()),
+			},
+		])
+		.then(({ startGame }) => startGame);
+	handleInitialInput(option);
+})();
--- app/src/models/Game.js
+++ app/src/models/Game.js
@@ -22,6 +22,7 @@ class Game {
 	#dealCard() {
 		const randomCard = deck.pickRandomCard();
 		this.#dealtCards.push(randomCard);
+		this.#player.score = this.#sumDealtCards();
 		return randomCard;
 	}
 
@@ -47,35 +48,61 @@ class Game {
 		return NumberUtils.isBetweenRange(this.#player.score, 18, 21);
 	}
 
-	start() {
+	async start() {
 		this.#initAttributes();
 		this.#dealInitialCards();
 		this.#player.score = this.#sumDealtCards();
 		this.#player.printScore();
 		if (this.#checkVictory()) {
 			console.log("Flawless victory! Blackjack!");
+			return this.exit();
 		}
 		this.#gameplay();
 	}
 
 	#stand() {
-		console.log("standing");
+		console.log("finishing game...");
+		this.#player.printScore();
+		if (this.#checkVictory()) {
+			console.log(`Your score is ${this.#player.score}, Blackjack!`);
+		}
+		console.log("Your score didn't reach the limit... You lost!");
 	}
 
 	#hit() {
-		console.log("hitting");
+		const card = this.#dealCard();
+		console.log(`Your dealt card is: ${card.toString()}`);
+		this.#player.printScore();
 	}
 
-	#gameplay() {
-		ConsoleUtils.readAndVerify(
-			"Do you want to stand or hit? (STAND/HIT)",
-			(option) => {
-				option.toLowerCase() === "stand" ? this.#stand() : this.#hit();
+	async #gameplay() {
+		const { choice } = await inquirer.prompt([
+			{
+				message: "Do you want to stand or hit?",
+				type: "list",
+				choices: ["Stand", "Hit"],
+				name: "choice",
 			},
-			(text) => /^(stand|hit)$/i.test(text.trim())
-		);
+		]);
+		const stood = choice.toLowerCase() === "stand";
+		stood ? this.#stand() : this.#hit();
+		if (!this.#checkVictory()) {
+			return stood && this.exit();
+			await this.#gameplay();
+		}
+		console.log("Blackjack!");
+		this.#player.printScore();
+		return this.exit();
+		// ConsoleUtils.readAndVerify(
+		// 	"Do you want to stand or hit? (STAND/HIT)",
+		// 	(option) => {
+		// 		option.toLowerCase() === "stand" ? this.#stand() : this.#hit();
+		// 		return this.#gameplay();
+		// 	},
+		// 	(text) => /^(stand|hit)$/i.test(text.trim())
+		// );
 		// stand or draw a card (loop)
-		// NumberUtils.betweenRange(18, 21, player.score) ? win() : checkIfGreaterThan21;
+		// NumberUtils.betweenRange(18, 21, player.score) ? win|() : checkIfGreaterThan21;
 		// won && playNextRound?
 		// round ++
 		// round === 3 && finishGame()
