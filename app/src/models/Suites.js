54a71d335298d17b1c05ebd7b368b9c0676d75ec
Jose Saúl Guerrero Serrano <josesaulserrano15@gmail.com>
2022-07-14 09:33:20 -0500

    Updated the JSDocs on Suite

--- app/src/models/Suites.js
+++ app/src/models/Suites.js
@@ -1,15 +1,22 @@
+// @ts-check
+
+/**
+ * @classdesc A class that represents the possible Suites in Poker deck.
+ * @class
+ */
 export class Suite {
 	#name;
 
 	/**
+	 * @description Constructs a new Suite from the possible options.
 	 * @param {"Spades ♠️" | "Clubs ♣️" | "Hearts ♥️" | "Diamonds ♦️"} name The name of the suite
-	 * @return {Suite} The instance that was just created
 	 */
 	constructor(name) {
 		this.#name = name;
 	}
 
 	/**
+	 * @description A getter that returns the name of the suite for this instance.
 	 * @return {String} The name of the suite assigned to this instance
 	 */
 	get name() {
